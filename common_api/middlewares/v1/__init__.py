7ae045980022ca91166bd57a336dab8e147e7879
Killian KOPP <killiankopp@gmail.com>
2025-06-20 16:03:09 +0200

    Rename CORSMiddleware to CustomCORSMiddleware in __init__.py

--- common_api/middlewares/v1/__init__.py
+++ common_api/middlewares/v1/__init__.py
@@ -2,4 +2,4 @@ from .exception_handler import http_exception_handler
 from .token_middleware import TokenVerificationMiddleware, read_cache_token, write_cache_token
 from .database_middleware import DBConnectionMiddleware
 from .licence_middleware import LicenceVerificationMiddleware
-from .cors_middleware import CORSMiddleware
+from .cors_middleware import CustomCORSMiddleware
