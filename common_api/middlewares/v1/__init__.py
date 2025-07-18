8b61191f3bc5a47f5ab8a16a7368ff342b9dfeeb
Killian KOPP <killiankopp@gmail.com>
2025-07-18 10:42:23 +0200

    Add StorageConnectionMiddleware import to __init__.py

--- common_api/middlewares/v1/__init__.py
+++ common_api/middlewares/v1/__init__.py
@@ -3,3 +3,4 @@ from .token_middleware import TokenVerificationMiddleware, read_cache_token, wri
 from .database_middleware import DBConnectionMiddleware
 from .licence_middleware import LicenceVerificationMiddleware
 from .cors_middleware import CustomCORSMiddleware
+from .storage_middleware import StorageConnectionMiddleware
